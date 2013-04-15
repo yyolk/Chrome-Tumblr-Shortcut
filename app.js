b650b9ebf60a3df3d42eca4fb82f33e13707df73
yyolk <input.output@gmail.com>
2013-04-15 01:10:06 -0500

    took out options. now simply does a redirect

--- app.js
+++ app.js
@@ -1,54 +1,18 @@
 (function($){
 
   var TumblrRedirect = function(){
-    this.tumblrUrl = "http://tumblr.com/";
+    this.tumblrUrl = "http://www.tumblr.com/";
   }
 
-  TumblrRedirect.prototype.setAccountID = function(accountID){
-    if(accountID) {
-      localStorage["bcAccountID"] = accountID;
-      return true;
-    } else {
-      $("#error").html("Non numeric ID not supported!");
-      return false;
-    }
-  }
-
-  TumblrRedirect.prototype.getAccountID = function(){
-    accountID = parseInt(localStorage["bcAccountID"]);
-    if(accountID) return accountID;
-  }
-
-  TumblrRedirect.prototype.save = function(){
-    if(this.setAccountID(parseInt($("#bc_account_id").val())))
-      this.redirect();
-  };
-
-  TumblrRedirect.prototype.load = function(){
-    $("#bc_account_id").val(this.getAccountID());
-  };
 
   TumblrRedirect.prototype.redirect = function(){
-    if(this.getAccountID()) {
-      window.location.replace(this.tumblrUrl + this.getAccountID());
-    } else {
-      window.location.replace("/options.html");
-    }
+    window.location.replace(this.tumblrUrl);
   }
 
-  TumblrRedirect.prototype.setup = function(){
-    var that = this;
-    if(window.location.pathname === "/options.html") {
-      $("#save_bc_account_id").on("click", function(ev){ that.save() });
-      this.load();
-    } else {
-      this.redirect();
-    }
-  }
 
   $(document).ready(function(){
     var tRedirect = new TumblrRedirect();
-    tRedirect.setup();
+    tRedirect.redirect();
   });
 
 })(jQuery);
